6823468e1c0c7231f5ad4c0117bd38061191ab8b
Magnus <magnus.maynard@greenroomrobotics.com>
2026-08-13 15:47:37 +1000

    Upgrading torch version, and migrating API (untested)

--- rtdetrv2_pytorch/src/data/transforms/_transforms.py
+++ rtdetrv2_pytorch/src/data/transforms/_transforms.py
@@ -57,8 +57,8 @@ class PadToSize(T.Pad):
         Mask,
         BoundingBoxes,
     )
-    def _get_params(self, flat_inputs: List[Any]) -> Dict[str, Any]:
-        sp = F.get_spatial_size(flat_inputs[0])
+    def make_params(self, flat_inputs: List[Any]) -> Dict[str, Any]:
+        sp = F.get_size(flat_inputs[0])
         h, w = self.size[1] - sp[0], self.size[0] - sp[1]
         self.padding = [0, 0, w, h]
         return dict(padding=self.padding)
@@ -69,7 +69,7 @@ class PadToSize(T.Pad):
         self.size = size
         super().__init__(0, fill, padding_mode)
 
-    def _transform(self, inpt: Any, params: Dict[str, Any]) -> Any:        
+    def transform(self, inpt: Any, params: Dict[str, Any]) -> Any:        
         fill = self._fill[type(inpt)]
         padding = params['padding']
         return F.pad(inpt, padding=padding, fill=fill, padding_mode=self.padding_mode)  # type: ignore[arg-type]
@@ -104,7 +104,7 @@ class ConvertBoxes(T.Transform):
         self.fmt = fmt
         self.normalize = normalize
 
-    def _transform(self, inpt: Any, params: Dict[str, Any]) -> Any:  
+    def transform(self, inpt: Any, params: Dict[str, Any]) -> Any:  
         spatial_size = getattr(inpt, _boxes_keys[1])
         if self.fmt:
             in_fmt = inpt.format.value.lower()
@@ -128,7 +128,7 @@ class ConvertPILImage(T.Transform):
         self.scale = scale
         self.single_channel = single_channel
 
-    def _transform(self, inpt: Any, params: Dict[str, Any]) -> Any:  
+    def transform(self, inpt: Any, params: Dict[str, Any]) -> Any:  
         inpt = F.pil_to_tensor(inpt)
         if self.dtype == 'float32':
             inpt = inpt.float()
